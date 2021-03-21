4ae49b40930fc0d256e67af59544eb14d49e99d7
github username <dirtyassweed@icloud.com>
2021-03-21 15:12:10 -0400

    adds header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Calculator } from './components/Calculator';
+import { Header } from './components/Header'
 import './css/App.css';
 
 const App = () => {
   return (
     <div className="app">
+      <Header />
       <Calculator />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/Header.css';
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <img src="https://i.ibb.co/27ZmQgV/Screen-Shot-2021-03-20-at-4-34-37-PM.png" alt="logo"/>
+            <h1>
+                Compound interest calculator designed specifically for traders.
+            </h1>
+        </div>
+    )
+}
+
