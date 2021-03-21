d35af00f6584ba882357dd3991bd20761c109a5f
github username <dirtyassweed@icloud.com>
2021-03-21 15:01:34 -0400

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import calculatorReducer from '../redux/calculatorSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    calculator: calculatorReducer,
   },
 });
---
+++ src/components/Calculator.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  incrementAsync,
+  selectCount,
+} from '../redux/calculatorSlice';
+
+export const Calculator = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState('2');
+
+  return (
+    <div className="calculator">
+    </div>
+  );
+}
